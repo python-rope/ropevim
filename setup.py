3a516dfee515b773561d95cfc0ccae5ffcad0211
Lie Ryan <lie.1296@gmail.com>
2022-07-21 14:36:28 +1000

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -9,9 +9,12 @@ except ImportError:
 
 classifiers = [
     'Development Status :: 6 - Mature',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Operating System :: OS Independent',
     'Environment :: X11 Applications',
     'Environment :: Win32 (MS Windows)',
